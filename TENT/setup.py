cdb28d19f79668eb392d735cf0eacec6e735a67a
Tianshuo Cong <90805377+tianshuocong@users.noreply.github.com>
2023-10-27 17:21:49 +0800

    Create setup.py

---
+++ TENT/setup.py
@@ -0,0 +1,84 @@
+import tent
+import os
+import torch
+from conf import cfg, load_cfg_fom_args
+import torch.optim as optim
+import numpy as np
+import random
+                
+def setseed(manualSeed) :
+    np.random.seed(manualSeed)
+    random.seed(manualSeed)
+    os.environ['PYTHONHASHSEED'] = str(manualSeed)
+    torch.manual_seed(manualSeed)
+    torch.cuda.manual_seed(manualSeed)
+    torch.cuda.manual_seed_all(manualSeed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+
+def setup_source(model):
+    """Set up the baseline source model without adaptation."""
+    model.eval()
+    logger.info(f"model for evaluation: %s", model)
+    return model
+
+
+def setup_norm(model):
+    """Set up test-time normalization adaptation.
+
+    Adapt by normalizing features with test batch statistics.
+    The statistics are measured independently for each batch;
+    no running average or other cross-batch estimation is used.
+    """
+    norm_model = norm.Norm(model)
+    logger.info(f"model for adaptation: %s", model)
+    stats, stat_names = norm.collect_stats(model)
+    logger.info(f"stats for adaptation: %s", stat_names)
+    return norm_model
+
+
+def setup_tent(model):
+    """Set up tent adaptation.
+
+    Configure the model for training + feature modulation by batch statistics,
+    collect the parameters for feature modulation by gradient optimization,
+    set up the optimizer, and then tent the model.
+    """
+    model = tent.configure_model(model)
+    params, param_names = tent.collect_params(model)
+    optimizer = setup_optimizer(params)
+    tent_model = tent.Tent(model, optimizer,
+                           steps=cfg.OPTIM.STEPS,
+                           episodic=cfg.MODEL.EPISODIC)
+    # logger.info(f"model for adaptation: %s", model)
+    # logger.info(f"params for adaptation: %s", param_names)
+    # logger.info(f"optimizer for adaptation: %s", optimizer)
+    return tent_model
+
+
+def setup_optimizer(params):
+    """Set up optimizer for tent adaptation.
+
+    Tent needs an optimizer for test-time entropy minimization.
+    In principle, tent could make use of any gradient optimizer.
+    In practice, we advise choosing Adam or SGD+momentum.
+    For optimization settings, we advise to use the settings from the end of
+    trainig, if known, or start with a low learning rate (like 0.001) if not.
+
+    For best results, try tuning the learning rate and batch size.
+    """
+    if cfg.OPTIM.METHOD == 'Adam':
+        return optim.Adam(params,
+                    lr=cfg.OPTIM.LR,
+                    betas=(cfg.OPTIM.BETA, 0.999),
+                    weight_decay=cfg.OPTIM.WD)
+    elif cfg.OPTIM.METHOD == 'SGD':
+        return optim.SGD(params,
+                   lr=cfg.OPTIM.LR,
+                   momentum=cfg.OPTIM.MOMENTUM,
+                   dampening=cfg.OPTIM.DAMPENING,
+                   weight_decay=cfg.OPTIM.WD,
+                   nesterov=cfg.OPTIM.NESTEROV)
+    else:
+        raise NotImplementedError
